7fc9260cea206b5aeb85a299e8222b9570d9af86
berstearns <bernardo.stearns@gmail.com>
2021-04-27 00:54:21 -0300

    fixed openapi

--- plot.py
+++ plot.py
@@ -1,3 +1,32 @@
+import pickle
+import os
+def load_devices():
+    devices = {}
+    with open("./private_devices/private_mobile_devices.csv") as inpf:
+        header = inpf.__next__().replace("\n","").split(",")
+        print(header)
+        for line in inpf:
+            timestamp_device = dict(zip(header,line.replace("\n","").split(",")))
+            if devices.get(timestamp_device["id_user"], False):
+                devices[timestamp_device["id_user"]]["timestamps"].append(timestamp_device)
+            else:
+                devices[timestamp_device["id_user"]] = {
+                            "id" : timestamp_device["id_user"], 
+                            "adjacents_nodes": [],
+                            "timestamps": [timestamp_device]
+                        }
+    return devices
+
+def list_matches(devices):
+    matches = {}
+    for device1_id in devices.keys():
+        for device2_id in devices.keys():
+            if (device1_id != device2_id):
+               if len(matches) >= 20:
+                  break 
+               matches = match(devices[device1_id], devices[device2_id], matches) 
+    return matches
+
 def match(device1, device2, matches):
     for t1 in device1['timestamps']:
         for t2 in device2['timestamps']:
@@ -15,56 +44,61 @@ def match(device1, device2, matches):
                     matches[match_id] =  [t1["timestamp_stop"]]
                     print(len(matches))
     return matches
+def create_graph(matches):
+    graph={}
+    for match_id in matches.keys():
+        if len(matches[match_id]) >= 1:
+            id1, id2 = match_id.split(":")
+            if graph.get(id1, False):
+                    graph[id1].append(id2)
+            else:
+                    graph[id1] = [id2]
+            if graph.get(id2, False):
+                    graph[id2].append(id1)
+            else:
+                    graph[id2] = [id1]
+    return graph
 
-devices = {}
-timestamps = []
-matches = {}
-degrees = {}
-graph = {}
-
-with open("./private_devices/private_mobile_devices.csv") as inpf:
-    header = inpf.__next__().replace("\n","").split(",")
-    print(header)
-    for line in inpf:
-        timestamp_device = dict(zip(header,line.replace("\n","").split(",")))
-        timestamps.append(timestamp_device["timestamp_start"])
-        timestamps.append(timestamp_device["timestamp_stop"])
-        if devices.get(timestamp_device["id_user"], False):
-            devices[timestamp_device["id_user"]]["timestamps"].append(timestamp_device)
+def create_degrees(graph):
+    degrees = {}
+    for node_id, adjacency_list in graph.items():
+        degree = len(adjacency_list)
+        if degrees.get(degree,False):
+            degrees[degree] +=1
         else:
-            devices[timestamp_device["id_user"]] = {
-                        "id" : timestamp_device["id_user"], 
-                        "adjacents_nodes": [],
-                        "timestamps": [timestamp_device]
-                    }
-    timestamps = set(timestamps)
+            degrees[degree] = 1
+    return degrees
 
-for device1_id in devices.keys():
-    for device2_id in devices.keys():
-        if (device1_id != device2_id):
-           if len(matches) >= 2000:
-              break 
-           matches = match(devices[device1_id], devices[device2_id], matches) 
 
-for match_id in matches.keys():
-    if len(matches[match_id]) >= 2:
-        id1,id2 = match_id.split(":")
-        if graph.get(id1, False):
-                graph[id1].append(id2)
-        else:
-                graph[id1] = [id2]
+if os.path.exists("devices.pickle"): 
+    devices = pickle.load(open("devices.pickle","rb"))
+else:
+    devices = load_devices()
+    with open("devices.pickle","wb") as devices_file:
+        pickle.dump(devices,devices_file)
 
-        if graph.get(id2, False):
-                graph[id2].append(id1)
-        else:
-                graph[id2] = [id1]
+if os.path.exists("matches.pickle"): 
+    devices = pickle.load(open("matches.pickle","rb"))
+else:
+    matches = list_matches(devices)
+    with open("matches.pickle","wb") as matches_file:
+        pickle.dump(matches, matches_file)
 
-for node_id, adjacency_list in graph.items():
-    degree = len(adjacency_list)
-    if degrees.get(degree,False):
-        degrees[degree] +=1
-    else:
-        degrees[degree] = 1
+if os.path.exists("graph.pickle"): 
+    graph = pickle.load(open("graph.pickle","rb"))
+else:
+    graph = create_graph(matches)
+    with open("graph.pickle","wb") as graph_file:
+        pickle.dump(graph,graph_file)
+
+if os.path.exists("degrees.pickle"): 
+    degrees = pickle.load(open("degrees.pickle","rb"))
+else:
+    degrees = create_degrees(graph)
+    with open("degrees.pickle","wb") as degrees_file:
+        pickle.dump(degrees,degrees_file)
+
+
 
 import seaborn
 from matplotlib.pyplot import show
